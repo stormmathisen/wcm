92683d4f99e3b185a09fc90ff18f64f63c7640bc
Storm <storm.mathisen@stfc.ac.uk>
2024-11-24 15:12:08 +0000

    Update test scripts to build in "build" directory

--- test.ps1
+++ test.ps1
@@ -1,3 +1,10 @@
-gcc -c -o wcm.dll wcm.c #Compile into library
-gcc -o test.exe test.c wcm.dll #Compile test program
-./test.exe #Run test program
+New-Item -Path . -Name "build" -ItemType "directory" -Force | Out-Null #Make build directory even if it exists
+gcc -c -o ./build/wcm.dll wcm.c #Compile into library
+gcc -o ./build/test.exe test.c ./build/wcm.dll #Compile test program
+Write-Output "======================================================================"
+Write-Output "C output"
+.\build\test.exe #Run test program
+Write-Output "======================================================================"
+Write-Output "Python output"
+python ./python_comp.py #Run python script
+Write-Output "======================================================================"
--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 #/bin/bash
-gcc -c -o wcm.o wcm.c #Compile into library
-gcc -o test test.c wcm.o #Compile test program
-./test #Run test program
+mkdir -p build #Create build directory
+gcc -c -o ./build/wcm.o wcm.c #Compile into library
+gcc -o ./build/test test.c wcm.o #Compile test program
+echo "=================================================="
+echo "C output"
+./build/test #Run test program
+echo "=================================================="
+echo "Python output"
+python ./python_comp.py #Run python script
+echo "=================================================="
