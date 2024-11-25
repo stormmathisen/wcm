543d334238673b6f932ae1f65cf987b7462cdfe0
Storm <storm.mathisen@stfc.ac.uk>
2024-11-25 12:43:46 +0000

    Fix linux test

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 mkdir -p build #Create build directory
-gcc -c -o ./build/wcm.o wcm.c #Compile into library
-gcc -o ./build/test test.c wcm.o #Compile test program
+gcc -c -o ./build/wcm.o wcm.c --std=c99 #Compile into library
+gcc -o ./build/test test.c ./build/wcm.o --std=c99 #Compile test program
 echo "=================================================="
 echo "C output"
 ./build/test #Run test program
 echo "=================================================="
 echo "Python output"
-python ./python_comp.py #Run python script
+python3 ./python_comp.py #Run python script
 echo "=================================================="
